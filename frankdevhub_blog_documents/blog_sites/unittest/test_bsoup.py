4feba4ee3433f1dc9632e24c1a0514832ed2fe65
frankdevhub <frankdevhub@gmail.com>
2021-09-05 18:20:22 +0800

    #commit:beautifulSoup测试抓取技术类博客文章单元测试

--- frankdevhub_blog_documents/blog_sites/unittest/test_bsoup.py
+++ frankdevhub_blog_documents/blog_sites/unittest/test_bsoup.py
@@ -6,9 +6,12 @@
 @Blog : http://blog.frankdevhub.site
 @Date ：2021/9/4 21:34
 """
-
 import logging as log
 import unittest
+
+import requests
+from bs4 import BeautifulSoup
+from lxml import etree
 
 log.basicConfig(level=log.DEBUG)
 
@@ -16,6 +19,8 @@ test_headers = {
     'Connection': 'close',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
 }
+
+test_51cto_blog_example = "https://blog.51cto.com/oldboy"  # https://blog.51cto.com/oldboy
 
 
 class TestBeautifulSoup(unittest.TestCase):
@@ -40,6 +45,21 @@ class TestBeautifulSoup(unittest.TestCase):
         # soup = BeautifulSoup(open('index.html'))  # 使用本地文件创建对象
         # log.debug(soup.prettify())
 
+    @staticmethod
+    def test_51cto_blog_example():
+        """测获取职位信息对应企业的介绍信息"""
+        log.debug('invoke method -> test_51cto_blog_example()')
+        response = requests.get(url=test_51cto_blog_example, headers=test_headers)
+        page_context = response.text
+        log.debug(page_context)
+
+        tree = etree.HTML(page_context)
+        assert tree is not None, 'xml tree cannot be empty'
+
 
 if __name__ == '__main__':
-    unittest.main()
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(TestBeautifulSoup('test_local'))  # test_local
+    test_suite.addTest(TestBeautifulSoup('test_51cto_blog_example'))  # test_51cto_blog_example
+    runner = unittest.TextTestRunner()
+    runner.run(test_suite)
