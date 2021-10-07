084cf79df8a73c864a9963bab42b726e851f15b7
frankdevhub <frankdevhub@gmail.com>
2021-10-07 23:03:14 +0800

    #commit: 测试正则表达式匹配博客文章链接的唯一标识

--- blog_sites/unittest/test_regex_expression.py
+++ blog_sites/unittest/test_regex_expression.py
@@ -19,11 +19,9 @@ eg:
    input = https://blog.51cto.com/oldboy/1926142 
    output = 1926142 
 """
-BLOG_DOC_ID_REGEX1 = """
-.*(?P<header>/http[s]{0,1}:\/\/([\w.]+\/?)\S*/)(?P<union>[0-9]+)$
+BLOG_DOC_ID_REGEX = """
+.*(?P<header>/http[s]{0,1}:\/\/([\w.]+\/?)\S*/)(?P<union_id>\/[0-9]{1,})$
 """
-
-BLOG_DOC_ID_REGEX = "(oldboy)"
 TEST_BLOG_DOC_LINK = "https://blog.51cto.com/oldboy/1926142"  # 测试博客网址链接(51CTO平台)
 TEST_BLOG_DOC_LINKS = ["https://blog.51cto.com/oldboy/1926142", "https://blog.51cto.com/oldboy/1884326",
                        "https://blog.51cto.com/oldboy/1855640", "https://blog.51cto.com/oldboy/775056",
@@ -41,16 +39,20 @@ class TestRegexExpression(unittest.TestCase):
            output = 1189530(文档唯一识别号)
         """
         log.debug('invoke method -> test_match_blog_union_id()')
-        matched = re.match(r'51cto', TEST_BLOG_DOC_LINK, re.I | re.M)
+        test_example = TEST_BLOG_DOC_LINK
+        print(f'test_blog_doc_link: {test_example}')
+        # matched = re.search(r'\/[0-9]{1,}$', test_example, re.M | re.I)
+        matched = re.search(BLOG_DOC_ID_REGEX, test_example, re.M | re.I)
         if matched:
-            print(matched.group())
-            print(f'union id : {matched.group("union_id")}')  # union_id
+            print(f'result: {matched.group()}')
+            # print(f'union_id: {matched.group("union_id")}')  # union_id
         else:
-            print('not matched')
+            print('result: not matched')
 
     @staticmethod
     def test_match_blog_union_ids():
         """测试逐个匹配51cto博客测试链接集合,匹配博文的唯一标识"""
+        log.debug('invoke method -> test_match_blog_union_ids()')
         for link in TEST_BLOG_DOC_LINKS:
             print(link)
         pass
