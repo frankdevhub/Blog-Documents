3bf44a90bab81b9ceb017bf7d565eeeb0f0ad06e
frankdevhub <frankdevhub@gmail.com>
2021-10-11 19:44:28 +0800

    #commit: 教育学历文化水平

--- blog_sites/tools/education.py
+++ blog_sites/tools/education.py
@@ -7,19 +7,27 @@
 @Date ：2021/2/19 19:22
 """
 
+import inspect
 from enum import Enum, unique
+
 
 @unique
 class EducationDegree(Enum):
-    """教育学历文化水平"""
-
+    # 教育学历文化水平
     def __new__(cls, args):
         instance = object.__new__(cls)
         instance.name = args['name']  # 学历中文名称
         instance.code = args['code']  # 学历英文代码
         return instance
 
-DOCTOR = {'name': '博士', 'code': 'doctor'}
-MASTER = {'name': '硕士研究生', 'code': 'master'}
-BACHELOR = {'name': '本科', 'code': 'bachelor'}
-TACHNICAL_COLLEGE = {'name': '中专', 'code': 'technical_college'}
+
+DOCTOR = {'name': '博士', 'code': 'doctor'}  # DOCTOR
+MASTER = {'name': '硕士研究生', 'code': 'master'}  # MASTER
+BACHELOR = {'name': '本科', 'code': 'bachelor'}  # BACHELOR
+TACHNICAL_COLLEGE = {'name': '中专', 'code': 'technical_college'}  # TACHNICAL_COLLEGE
+
+if __name__ == '__main__':
+    for enum_instance in EducationDegree.__members__:
+        print(f'{enum_instance}')
+        for (type_name, obj) in inspect.getmembers(enum_instance):
+            print(f'type_name: {type_name}, obj: {obj}')
