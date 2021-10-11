97d91ad4728b68073d115f00b022b22dd18ab171
frankdevhub <frankdevhub@gmail.com>
2021-10-11 19:36:29 +0800

    #commit: 通用国际字符编码

--- blog_sites/tools/encode.py
+++ blog_sites/tools/encode.py
@@ -7,22 +7,30 @@
 @Date ：2021/2/16 20:49
 """
 
+import inspect
 from enum import Enum, unique
+
 
 @unique
 class CharacterEncode(Enum):
-    """通用国际字符编码"""
-
+    # 通用国际字符编码
     def __new__(cls, name: str):
         instance = object.__new__(cls)
         instance.code_name = name
         return instance
 
-    GB2312 = 'GB2312'
-    ASCII = 'ASCII'
-    MBCS = 'MBCS'
-    GBK = 'GBK'
-    Big5 = 'Big5'
-    Unicode = 'Unicode'
-    UTF8 = 'UTF8'
-    Base64 = 'Base64'
+    GB2312 = 'GB2312'  # GB2312
+    ASCII = 'ASCII'  # ASCII
+    MBCS = 'MBCS'  # MBCS
+    GBK = 'GBK'  # GBK
+    Big5 = 'Big5'  # Big5
+    Unicode = 'Unicode'  # Unicode
+    UTF8 = 'UTF8'  # UTF8
+    Base64 = 'Base64'  # Base64
+
+
+if __name__ == '__main__':
+    for instance in CharacterEncode.__members__:
+        print(f'{instance}')
+        for (type_name, obj) in inspect.getmembers(instance):
+            print(f'type_name: {type_name}, obj: {obj}')
