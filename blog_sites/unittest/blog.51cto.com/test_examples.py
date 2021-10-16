206d62f31e219e5e634a2fcccbc15667f0d5e90e
frankdevhub <frankdevhub@gmail.com>
2021-10-16 22:27:27 +0800

    #commit: 测试解析博客文档页面,获取文档的标题，摘要,正文内容,分类标签等其他相关属性值

--- blog_sites/unittest/blog.51cto.com/test_examples.py
+++ blog_sites/unittest/blog.51cto.com/test_examples.py
@@ -4,7 +4,7 @@
 @File ：test_examples.py
 @Author ：frankdevhub@gmail.com
 @Blog : http://blog.frankdevhub.site
-@Date ：2021/9/4 21:34
+@Date ：2021/10/8 22:14
 """
 import logging as log
 import unittest
@@ -17,11 +17,13 @@ test_headers = {
     'Connection': 'close',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
 }
-test_blog_example = "https://blog.51cto.com/oldboy"  # https://blog.51cto.com/oldboy 博客个人空间页面
+test_blog_example = "https://blog.51cto.com/oldboy"  # https://blog.51cto.com/oldboy 博客个人空间页面(测试样例)
+test_doc_link = "https://blog.51cto.com/oldboy/1189530"  # https://blog.51cto.com/oldboy/1189530  博客文档内容页面(测试样例)
 test_docs_list_xpath = "//div[@class='common-article-list']"  # //div[@class="common-article-list"] 博客个人空间页面下当前页下的文档对象集合
 test_pagination_tags_xpath = "//ul[@class='pagination']/li/a"  # //ul[@class='pagination']/li/a 博客个人空间当前页页脚分页控件对象
 
 
+# 测试获取博客文档以及相关属性
 class TestExamples(unittest.TestCase):
 
     @staticmethod
@@ -77,6 +79,30 @@ class TestExamples(unittest.TestCase):
             # href
             print(f'attribute href = {tag.get("href")}')
 
+    @staticmethod
+    def doc_tree_example():
+        # 获取测试博客文档页面的DOM树对象
+        log.debug('invoke method -> doc_tree_example()')
+        # 测试博客文档连接:test_doc_link
+        response = requests.get(url=test_doc_link, headers=test_headers)
+        page_context = response.text
+        tree = etree.HTML(page_context)
+        assert tree is not None, 'xml tree cannot be none'
+        # print(type(tree))
+        return tree
+
+    # https://blog.51cto.com/oldboy/1189530
+    @staticmethod
+    def test_get_document_properties():
+        # 测试获取文档对象的各个属性
+        log.debug('invoke method -> test_get_document_properties()')
+        page_tree = doc_tree_example()
+        # 测试: 获取文档对象大标题 eg: LDAP跨多机房统一认证及授权管理精品解决方案
+        # 测试: 获取文档摘要内容
+        # 测试: 获取文档正文内容
+        # 测试: 获取完整文档的页面HTML页面
+        # 测试: 获取文档的创建时间,文档的分类标签,分类类别名称
+        pass
 
 if __name__ == '__main__':
     test_suite = unittest.TestSuite()
